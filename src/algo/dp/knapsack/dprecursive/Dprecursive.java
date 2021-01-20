39837cbef673d7f00a5cf09c3e7240c493341c66
Praveen Kumar <praveen.eee17@nituk.ac.in>
2021-01-21 02:39:57 +0530

    Add Knapsack DP wth Recursive

--- src/algo/dp/knapsack/dprecursive/Dprecursive.java
+++ src/algo/dp/knapsack/dprecursive/Dprecursive.java
@@ -1,4 +1,27 @@
 package algo.dp.knapsack.dprecursive;
 
 public class Dprecursive {
+    private static int[][] Matrix = new int[100][100];
+    public int maxpro(int[] wt, int[] pro, int w){
+        for(int i = 0; i < 100; i++){
+            for(int j = 0; j < 100; j++){
+                Matrix[i][j] = -1;
+            }
+        }
+        int n = wt.length;
+        return maxprofit(wt, pro, n, w);
+    }
+    private int maxprofit(int[] wt, int[] pro, int n, int w){
+        if(w == 0 || n == 0) return 0;
+        if(Matrix[n][w] != -1) {
+            return Matrix[n - 1][w - 1];
+        }
+        if(wt[n - 1] > w){
+            return Matrix[n][w] = maxprofit(wt, pro, n - 1, w);
+        }
+        else{
+            return Matrix[n][w] = Math.max( pro[n - 1] + maxprofit(wt, pro, n - 1, w - wt[n -1]), maxprofit(wt, pro, n - 1, w));
+        }
+
+    }
 }
--- src/algo/dp/knapsack/dprecursive/DprecursiveTest.java
+++ src/algo/dp/knapsack/dprecursive/DprecursiveTest.java
@@ -1,4 +1,24 @@
 package algo.dp.knapsack.dprecursive;
 
+import algo.dp.knapsack.dppure.Puredynamic;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DprecursiveTest {
+    @Test
+    void passAll() {
+        Dprecursive o = new Dprecursive();
+        int profit = o.maxpro(new int[]{1, 3, 2}, new int[]{1, 3, 2}, 5);
+        assertEquals(5, profit);
+
+        profit = o.maxpro(new int[]{1, 1, 1}, new int[]{1, 3, 2}, 2);
+        assertEquals(5, profit);
+
+        profit = o.maxpro(new int[]{1, 3, 4, 2}, new int[]{4, 2, 2, 5}, 5);
+        assertEquals(9, profit);
+
+        profit = o.maxpro(new int[]{10, 20, 30}, new int[]{60, 100, 120}, 50);
+        assertEquals(220, profit);
+    }
 }
