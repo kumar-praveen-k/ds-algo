788417af76a461045e277856f91217afac87542c
Praveen Kumar <praveen.eee17@nituk.ac.in>
2021-01-21 02:59:30 +0530

    Add Knapsack pure DP

--- src/algo/dp/knapsack/dppure/Puredynamic.java
+++ src/algo/dp/knapsack/dppure/Puredynamic.java
@@ -1,4 +1,33 @@
 package algo.dp.knapsack.dppure;
 
 public class Puredynamic {
+    private static int[][] Matrix = new int[100][100];
+    public int maxpro(int[] wt, int[] pro, int w){
+        for(int i = 0; i < 100; i++){
+            for(int j = 0; j < 100; j++){
+                if(i == 0 || j == 0) Matrix[i][j] = 0;
+            }
+        }
+        int n = wt.length;
+        return maxprofit(wt, pro, n, w);
+    }
+    private int maxprofit(int[] wt, int[] pro, int n, int w){
+        for(int i = 0; i < n+1; i++){
+            for(int j = 0; j < w+1; j++){
+                if(i == 0 || j == 0){
+                    Matrix[i][j] = 0;
+                }
+            }
+        }
+        for(int i = 1; i <= n; i++) {
+            for (int j = 1; j <= w; j++) {
+                if (wt[i - 1] > j) {
+                    Matrix[i][j] = maxprofit(wt, pro, i - 1, j);
+                } else {
+                    Matrix[n][w] = Math.max(pro[i - 1] + maxprofit(wt, pro, i - 1, j - wt[i - 1]), maxprofit(wt, pro, i - 1, j));
+                }
+            }
+        }
+        return Matrix[n][w];
+    }
 }
--- src/algo/dp/knapsack/dppure/PuredynamicTest.java
+++ src/algo/dp/knapsack/dppure/PuredynamicTest.java
@@ -1,4 +1,24 @@
 package algo.dp.knapsack.dppure;
 
+import algo.dp.knapsack.dprecursive.Dprecursive;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PuredynamicTest {
+    @Test
+    void passAll() {
+        Puredynamic o = new Puredynamic();
+        int profit = o.maxpro(new int[]{1, 3, 2}, new int[]{1, 3, 2}, 5);
+        assertEquals(5, profit);
+
+        profit = o.maxpro(new int[]{1, 1, 1}, new int[]{1, 3, 2}, 2);
+        assertEquals(5, profit);
+
+        profit = o.maxpro(new int[]{1, 3, 4, 2}, new int[]{4, 2, 2, 5}, 5);
+        assertEquals(9, profit);
+
+        profit = o.maxpro(new int[]{10, 20, 30}, new int[]{60, 100, 120}, 50);
+        assertEquals(220, profit);
+    }
 }
